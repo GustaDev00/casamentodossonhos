ada3d485d30d2ecd1869e5541584d7401f48a5b5
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-10-20 19:41:51 -0300

    as

--- view/loja/listaproduto.php
+++ view/loja/listaproduto.php
@@ -16,7 +16,7 @@ $pagina = 1;
         $pl = (10 * ($pagina - 1)) + 1;
         $npl = $pagina * 10;
         for($i=$pl;$i<=$npl;$i++){
-             $verificar2 = "SELECT * FROM produto where produtoID = {$i}";
+             $verificar2 = "SELECT * FROM produto where cod_produto = {$i}";
                 $querySelect = executeSelect($conn, $verificar2);
                 if($querySelect->rowCount() > 0){
                     while($fetch = $querySelect->fetch(PDO::FETCH_ASSOC)){
