68439c9ca5312b30c460483ba86829543f579181
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-10-20 18:53:20 -0300

    asas

---
+++ deletes/list_pres.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../Db/daohelper.php';
+session_start();
+
+try{
+    if(empty($_POST)){
+        echo "vazio tio";
+    }else{
+        $id = $_SESSION['id'];
+        $nomePres = isset($_REQUEST['nomePres'])?$_REQUEST['nomePres']:null;
+        $conn = connection();
+        $delete = "DELETE FROM LISTA_PRESENTES WHERE NOME_VALOR_PRESENTE = '$nomePres' AND COD_USU = '$id'";
+        $executeDelete = executeQuery($conn, $delete);
+        echo "<script>location.href='../perfil.php'</script>";
+    }
+    
+    
+
+} catch (Exception $ex) {
+
+}
