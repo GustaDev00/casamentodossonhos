8071b59040edeb0ff11ad03a23bcae4d60393ed9
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-10-21 17:57:32 -0300

    okay

--- view/perfil_vendedor/index.php
+++ view/perfil_vendedor/index.php
@@ -9,7 +9,7 @@
         }else{
             include_once 'index.html';
             
-        include_once '../mask/footer.html';
+       
         }
-          
+        include_once '../mask/footer.html';
     ?>
