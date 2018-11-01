999953c10626a6b6c2b459ebb984024c2bec6dfd
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-11-01 01:04:36 -0300

    nada de mais

--- view/_js/perfil.js
+++ view/_js/perfil.js
@@ -193,27 +193,6 @@ function criaimg(){
         $('#fundoE').fadeTo("fast",0.8);
     }
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-        }
-        
-        function currentSlide(n) {
-        showSlides(slideIndex = n);
-        }
-        
-        function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        }
-        slides[slideIndex-1].style.display = "block";
-        captionText.innerHTML = dots[slideIndex-1].alt;
-        }
-
         function functionmy(xyz){   
             var fu = ".Pcampo:nth-child(" + xyz + ") img";
             var sr = $(fu).attr("src");
@@ -226,3 +205,24 @@ function criaimg(){
             $("#fundoP").show();
             $(".modinha").show();
         }
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+            }
+            
+            function currentSlide(n) {
+            showSlides(slideIndex = n);
+            }
+            
+            function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var captionText = document.getElementById("caption");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+            }
+            slides[slideIndex-1].style.display = "block";
+            captionText.innerHTML = dots[slideIndex-1].alt;
+            }
