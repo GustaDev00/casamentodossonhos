4eb795ba7bdd4b4196e955f0e75f93cdf08e163b
Nathan Garcia <nathangar01@gmail.com>
2018-11-03 21:03:16 -0300

    Editar da Foto, do nome e da descrição do vendedor.

---
+++ view/updates/descricao_vendedor.php
@@ -0,0 +1,69 @@
+<?php
+require_once '../../Db/daohelper.php';
+session_start();
+$conn = connection();
+
+try{
+    if(empty($_FILES or $_POST)){
+        echo'<script>';
+        echo"location.href='index.html'";
+        echo '</script>';
+    }else{
+        $cat = isset($_POST['novaCategoria'])?$_POST['novaCategoria']:null;
+        $tel = isset($_POST['novoTel'])?$_POST['novoTel']:null;
+        $rua = isset($_POST['novaRua'])?$_POST['novaRua']:null;
+        $bairro = isset($_POST['novoBairro'])?$_POST['novoBairro']:null;
+        $cidade = isset($_POST['novaCidade'])?$_POST['novaCidade']:null;
+        $estado = isset($_POST['novoEstado'])?$_POST['novoEstado']:null;
+        $id = $_SESSION['id'];
+
+    if($cat == 'Nova Categoria' or empty($cat)){}else{
+        $upC = "update empresa
+                set categoria_empre = '$cat'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upC);
+    }
+
+    if(empty($tel)){}else{
+        $upT = "update empresa
+                set tel_empre = '$tel'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upT);
+    }
+
+    if(empty($rua)){}else{
+        $upR = "update empresa
+                set rua_empre = '$rua'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upR);
+    }
+
+    if(empty($bairro)){}else{
+        $upB = "update empresa
+                set bairro_empre = '$bairro'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upB);
+    }
+
+    if(empty($cidade)){}else{
+        $upC = "update empresa
+                set cidade_empre = '$cidade'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upC);
+    }
+
+    if(empty($estado)){}else{
+        $upE = "update empresa
+                set estado_empre = '$estado'
+                where cod_empresa = '$id';";
+        $execC = executeQuery($conn, $upE);
+    }
+
+    echo'<script>';
+    echo"location.href='../perfil/perfil.php'";
+    echo '</script>';
+
+    }
+}catch (Exception $ex) {
+
+}
---
+++ view/updates/img_nome_perfil_vendedor.php
@@ -0,0 +1,51 @@
+<?php
+include_once '../../Db/daohelper.php';
+session_start();
+try{
+    if(empty($_FILES)){
+        echo'<script>';
+        echo"location.href='index.html'";
+        echo '</script>';
+    }else{
+        $img = isset($_FILES['fotoPV'])?$_FILES['fotoPV']:null;
+        $nomePV = isset($_POST['nomePV'])?$_POST['nomePV']:null;
+        $id = $_SESSION['id'];
+        $conn = connection();
+if(empty($img or $_FILES['fotoPV'])){echo "vazio";}else{
+    
+   $arquivo_tmp = $img[ 'tmp_name' ];
+   $nome = $img[ 'name' ];
+   $extensao = pathinfo ( $nome, PATHINFO_EXTENSION );
+   $extensao = strtolower ( $extensao );
+ if(empty($extensao)){}else{
+       if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
+           $novoNome = uniqid ( time () ) . '.' . $extensao;
+           $destino = '../_imagem_usuario/'.$novoNome;
+          
+           $upE = "update empresa
+           set foto_empre='$destino'
+           where cod_empresa = '$id';";
+                   $executeE = executeQuery($conn, $upE);
+            @move_uploaded_file ( $arquivo_tmp, "$destino" );
+
+            
+        
+       }
+   }
+}
+
+if(empty($_POST['nomePV'] or $nomePV)){}else{
+
+    $upPV = "update empresa
+            set nome_empre ='$nomePV'
+            where cod_empresa = '$id';";
+            $executeN = executeQuery($conn, $upPV);
+
+}
+            echo'<script>';
+            echo"location.href='../perfil/perfil.php'";
+            echo '</script>';
+    }
+}catch (Exception $ex) {
+
+}
