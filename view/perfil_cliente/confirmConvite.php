92555c56c41d8f05cfcbc30db62f18bd265bf3ea
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-11-07 00:41:37 -0200

    auosdhausdh

--- view/perfil_cliente/confirmConvite.php
+++ view/perfil_cliente/confirmConvite.php
@@ -8,40 +8,42 @@ try{
         echo "ta vazio";
     }else{
         $emailConvidado = isset($_POST['confirmEmail'])?$_POST['confirmEmail']:null;
-        echo $emailConvidado;
-        $id = $_GET['cod'];
-        $idPres = $_GET['?pres'];
-        echo $idPres .'<br>';
-        echo $id;
+        //echo $emailConvidado;
+        $id = isset($_REQUEST['idD'])?$_REQUEST['idD']:NULL;
+        $idPres = isset($_REQUEST['iDD'])?$_REQUEST['iDD']:NULL;
         $conn = connection();
         $select = "select * from convidados where email_conv = '$emailConvidado' and cod_usu = '$id';";
         $executeSelect = executeSelect($conn, $select);
-        $fetch4 = $executeSelect->fetch(PDO::FETCH_OBJ);
-        $idConv = $fetch4->cod_conv;
-        
+
         if($executeSelect->rowCount() > 0){
+            $fetch4 = $executeSelect->fetch(PDO::FETCH_OBJ);
+            $idConv = $fetch4->cod_conv;
             $verific = "select * from lista_presentes where cod_list_pres = '$idPres' and status_presente = 'Confirmado';";
             $executeVerific = executeSelect($conn, $verific);
             if($executeVerific->rowCount() > 0){
                 echo '<script>';
-                echo 'alert("Alguem já irá dar este Presente! 
-                            Por favor, Selecione Outro Presente!")';
+                echo 'alert("Alguem já irá dar este Presente! Por favor, Selecione Outro Presente!")';
                 echo '</script>';
-                echo "alguem ja vai da o presente seu lixo";
+                echo '<script>';
+                echo "location.href='javascript:history.go(-1)'";
+                echo '</script>';
                 
             }else{
-           $update = "update lista_presentes set status_presente = 'Confirmado', COD_CONV = '$idConv' where cod_usu = '$id'";
+           $update = "update lista_presentes set status_presente = 'Confirmado', COD_CONV = '$idConv' where cod_usu = '$id' and  cod_list_pres = '$idPres' ";
            $sql = executeQuery($conn, $update);
+           echo '<script>';
+           echo "location.href='javascript:history.go(-1)'";
+           echo '</script>';    
             }
         }else{
+            echo "asdads";
             echo '<script>';
-            echo 'alert("Você ainda não está na lista de convidados! <br>
-                        Por favor Confirme a Presença Primeiro!")';
+            echo 'alert("Você ainda não está na lista de convidados! Por favor Confirme a Presença Primeiro!")';
             echo '</script>';
 
-           /* echo '<script>';
-            echo "location.href='../index.php.'";
-            echo '</script>';*/
+            echo '<script>';
+            echo "location.href='javascript:history.go(-1)'";
+            echo '</script>';
         }
     }
 
