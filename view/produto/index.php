bf5cf087923356749696e88988b040fc15866113
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-11-10 10:47:13 -0200

    war

--- view/produto/index.php
+++ view/produto/index.php
@@ -1,6 +1,6 @@
 <?php
     include_once '../../Db/daohelper.php';
-    
+    session_start();
     $idProd = $_GET['cod'];
     include_once '../mask/header.html';
     if(empty($idProd)){
@@ -10,7 +10,7 @@
         $select = "   select p.nome_prod, p.preco_prod, p.desc_prod, p.iz_prod, 
         p.local_prod, p.url_foto_prod, 
         c.nome_categoria, c.desc_categoria,
-        e.nome_empre, e.cidade_empre, e.estado_empre, e.tel_empre, e.foto_empre
+        e.nome_empre, e.email_empre, e.cidade_empre, e.estado_empre, e.tel_empre, e.foto_empre
         from produto p
         inner join categoria c
         on p.cod_categoria = c.cod_categoria 
@@ -26,6 +26,7 @@
         $loc = $fetch->local_prod;
         $sprod = $fetch->iz_prod;
         $nomeE = $fetch->nome_empre;
+        $emailE = $fetch->email_empre;
         $locC = $fetch->cidade_empre;
         $locEst = $fetch->estado_empre;
         $contato = $fetch->tel_empre;
