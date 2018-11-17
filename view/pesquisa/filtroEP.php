e92b825f7f62d7124ce9ce6b9136899f64d0ec87
Gustavo Felipe <gustavo.felipe01@outlook.com>
2018-11-17 19:20:02 -0200

    zxdcfvghbnjkm,l

---
+++ view/pesquisa/filtroEP.php
@@ -0,0 +1,69 @@
+<?php
+if($checkbox != NULL){
+    if($nomeF != NULL){
+        $nomeF = strtolower($nomeF);
+        $arry2 = array();
+        foreach($checkbox as $valor){
+            $xcont = 0;
+            if($valor == "empresa"){
+                for($x=0; $x < $CN ; $x++){
+                    $nomeMini =strtolower($nome[$x]['nome_empre']);
+                    if($nomeF ==  $nomeMini){   
+                       $arry2[] = $nome[$x]['nome_empre']. ".empresa,$x";
+                    }else if( strstr($nomeMini,$nomeF)){
+                        $arry2[] = $nome[$x]['nome_empre']. ".empresa,$x";
+                    }else{}
+                     }
+            }else{}
+            if($valor == "produto"){
+                for($l = 0; $l < $CR; $l++){
+                    $nomeMini =strtolower($produto[$l]['nome_prod']);
+                    if($nomeF ==  $nomeMini){
+                        $arry2[] = $produto[$l]['nome_prod']. ".produto,$l";
+                    }else if( strstr($nomeMini,$nomeF)){
+                        $arry2[] = $produto[$l]['nome_prod']. ".produto,$l";
+                    }else{}
+                    }
+            }else{}        
+        }
+    }else{
+        $arry2 = array();
+        foreach($checkbox as $valor){
+            $xcont = 0;
+            if($valor == "empresa"){
+                for($x=0; $x < $CN ; $x++){
+                    $arry2[] = $nome[$x]['nome_empre']. ".empresa,$x";
+                     }
+            }else{}
+            if($valor == "produto"){
+                for($l = 0; $l < $CR; $l++){
+                    $arry2[] = $produto[$l]['nome_prod']. ".produto,$l";
+                    }
+            }else{}        
+        }
+    }
+   
+}else{}
+    if($nomeF != NULL and $checkbox == NULL){
+        $nomeF = strtolower($nomeF);
+        $arry2 = array();
+        for($x=0; $x < $CN ; $x++){
+            $nomeMini =strtolower($nome[$x]['nome_empre']);
+            if($nomeF ==  $nomeMini){   
+               $arry2[] = $nome[$x]['nome_empre']. ".empresa,$x";
+            }else if( strstr($nomeMini,$nomeF)){
+                $arry2[] = $nome[$x]['nome_empre']. ".empresa,$x";
+            }else{}
+             }
+             for($l = 0; $l < $CR; $l++){
+                $nomeMini =strtolower($produto[$l]['nome_prod']);
+                if($nomeF ==  $nomeMini){
+                    $arry2[] = $produto[$l]['nome_prod']. ".produto,$l";
+                }else if( strstr($nomeMini,$nomeF)){
+                    $arry2[] = $produto[$l]['nome_prod']. ".produto,$l";
+                }else{}
+                }
+    }else{}
+    
+        uasort($arry2, "strnatcmp");
+ ?>
--- view/pesquisa/index.php
+++ view/pesquisa/index.php
@@ -2,14 +2,20 @@
         require_once '../../Db/daohelper.php';
         session_start();
         include_once '../mask/header.html';
-        
+        $nomeF = isset($_POST["nomeF"])?$_POST["nomeF"]:NULL;
+        $checkbox = isset($_POST["escolhaC"])?$_POST["escolhaC"]:NULL;
+        $pesquisa = isset($_GET['pesquisar'])?$_GET['pesquisar']:NULL;
+        if(($checkbox != NULL) or ($nomeF != NULL)){
+          include_once 'pesquisa.php';
+          include_once 'filtroEP.php';
+        }else{
         include_once 'pesquisa.php';
-    
+        if((isset($_SESSION['pesquisado'])) and ($_SESSION['pesquisado'] == $pesquisa)){
+        }else{
+        $_SESSION['pesquisado'] = $pesquisa;
+      }
+        }
         include_once 'pesquisa.html';
 
         include_once '../mask/footer.html';
-       
-      /*
-       
-                    */    
     ?>
